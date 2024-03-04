bc53397674b806ce41a41bdbd59654e058768981
yairme <yairstward2013@gmail.com>
2024-03-04 15:38:17 +0100

    Added WIP Velocity

--- CoinFlipper/Assets/CoinRotation.cs
+++ CoinFlipper/Assets/CoinRotation.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+/// <summary>
+/// Called every frame to check for user input and rotate the coin accordingly.
+/// </summary>
 public class CoinRotation : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject coinObject;
+    [SerializeField] private GameObject coinObject;
+    [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private Vector3 rotationVelocity;
 
-    private bool isRotating = false;
+    private bool isRotating;
     private Vector3 previousMousePosition;
 
+    /// <summary>
+    /// Checks for user input and updates the rotation of the coin.
+    /// </summary>
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -27,6 +33,7 @@ public class CoinRotation : MonoBehaviour
         else if (Input.GetMouseButtonUp(0))
         {
             isRotating = false;
+            rotationVelocity = new Vector3(-Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0f) * rotationSpeed;
         }
 
         if (isRotating)
@@ -38,6 +45,13 @@ public class CoinRotation : MonoBehaviour
             transform.Rotate(Vector3.right, -mouseDelta.y, Space.World);
 
             previousMousePosition = currentMousePosition;
+
+            // Decrease rotationVelocity until it reaches 0
+            rotationVelocity = Vector3.Lerp(rotationVelocity, Vector3.zero, Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(rotationVelocity * Time.deltaTime, Space.World);
         }
     }
 }
